b1a715f737d99bd17fc3079fb33cc8beee669dd1
Anushkinz <anuarprogramm@gmail.com>
2022-03-06 17:15:19 +0600

    Initial Commit

---
+++ VKNewsApp/FeedViewController/FeedViewController.swift
@@ -0,0 +1,22 @@
+//
+//  FeedViewController.swift
+//  VKNewsApp
+//
+//  Created by anushkinz on 5/3/22.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+  
+    }
+    
+
+
+
+
+}
